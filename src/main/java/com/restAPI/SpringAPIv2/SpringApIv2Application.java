7ecbcec37528d42dfc7b1d496d579fda3682f425
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2023-07-13 18:46:38 -0300

    Archivos iniciales

---
+++ src/main/java/com/restAPI/SpringAPIv2/SpringApIv2Application.java
@@ -0,0 +1,13 @@
+package com.restAPI.SpringAPIv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApIv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApIv2Application.class, args);
+	}
+
+}

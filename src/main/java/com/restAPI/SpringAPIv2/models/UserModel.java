e3b20d6282d52716c3aaec0df3cfd3c8b559554a
Franco Ezequiel Rodriguez <frrodriguez@fi.uba.ar>
2023-07-14 19:45:32 -0300

    Se crea el modelo con sus respectivos getters y setters

---
+++ src/main/java/com/restAPI/SpringAPIv2/models/UserModel.java
@@ -0,0 +1,44 @@
+package com.restAPI.SpringAPIv2.models;
+
+public class UserModel {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private int priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
